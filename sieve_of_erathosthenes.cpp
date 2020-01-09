4fdd02be09627d4499be196666946956967ed2b4
akshayr6 <akshayrajareddy6@gmail.com>
2020-01-09 23:01:48 +0530

    Update sieve_of_erathosthenes.cpp

--- sieve_of_erathosthenes.cpp
+++ sieve_of_erathosthenes.cpp
@@ -8,11 +8,11 @@ int main(){
 	fast;	
 	long long int n;
 	cin>>n;	
-	bool prime[n];
+	bool prime[n+1];
 	memset(prime,true,sizeof(prime));
-	for(int i=2;i*i<n;i++){
+	for(int i=2;i*i<=n;i++){
 		if(!prime[i]) continue;
-		for(int p=i*i;p<=n;p+=i){
+		for(int p=i*2;p<=n;p+=i){
 			if(prime[p])
 				prime[p]=false;
 			}
@@ -23,9 +23,7 @@ int main(){
 	return 0;
 	}
 // Step 1. Initiaslise a boolean array with index to specify the numbers
-// Step 2. Starting from 2 keep track of all the multiples that are greater
-//than the SQUARE of the selected prime because all the numbers before the
-//SQUARE of the numbers are already striked out
+// Step 2. Starting from 2 keep track of all the multiples (for eg: 4,6,8,... for 2 and 6,9,12,... for 3) and mark them false
 // Step 3. Run the main loop only until the SQUARE-ROOT of the selected
-//integer as the integeres greater than the SQUARE-ROOT are already checked
+//integer as the integers greater than the SQUARE-ROOT are already checked
 //for and are only repetetions 
